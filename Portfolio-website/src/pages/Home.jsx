f836ec9816566b9e3edeb4738072dcc33c20f857
Colin-Harwood <138476341+Colin-Harwood@users.noreply.github.com>
2024-01-06 21:04:58 +0200

    Added fonts

--- Portfolio-website/src/pages/Home.jsx
+++ Portfolio-website/src/pages/Home.jsx
@@ -3,7 +3,12 @@ import './Home.css'
 
 const Home = () => {
   return (
-    <div><h1>Please leave. YMCA <br /> Please</h1></div>
+    < >
+    <div><h1>Hi, I'm Colin.</h1>
+    <p>I'm a fullstack web designer based out of South Africa</p>
+    <h2>Hi, I'm Colin.</h2>
+    </div>
+    </>
   )
 }
 
