470c8d893a6f760fa5f09e66bfb6781fd127afdc
Colin-Harwood <138476341+Colin-Harwood@users.noreply.github.com>
2024-01-06 21:48:20 +0200

    Set up tailwind

--- Portfolio-website/src/pages/Home.jsx
+++ Portfolio-website/src/pages/Home.jsx
@@ -4,7 +4,7 @@ import './Home.css'
 const Home = () => {
   return (
     < >
-    <div><h1>Hi, I'm Colin.</h1>
+    <div><h1 className="text-3xl font-bold underline">Hi, I'm Colin.</h1>
     <p>I'm a fullstack web designer based out of South Africa</p>
     <h2>Hi, I'm Colin.</h2>
     </div>
