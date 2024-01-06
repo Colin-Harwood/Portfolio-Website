d1a89f8f0959bc8409023537d18c9e91815e8e77
Colin-Harwood <138476341+Colin-Harwood@users.noreply.github.com>
2024-01-06 23:23:49 +0200

    Adjusted some colour schemes as well as aligned the items  into two columns

--- Portfolio-website/src/pages/Home.jsx
+++ Portfolio-website/src/pages/Home.jsx
@@ -3,11 +3,18 @@ import './Home.css'
 
 const Home = () => {
   return (
-    < >
-    <div><h1 className="text-3xl font-bold underline">Hi, I'm Colin.</h1>
-    <p>I'm a fullstack web designer based out of South Africa</p>
-    <h2>Hi, I'm Colin.</h2>
-    </div>
+    <>
+      <div className="flex flex-row justify-center align-middle">
+        <div className="flex flex-col items-center basis-1/2 p-4"> 
+          <div className="text-left">
+            <h1 className="text-3xl font-bold">Hi, I'm Colin.</h1>
+            <p>I'm a fullstack web designer based out of South Africa</p>
+            <p>Test</p>
+            <p>Test2</p>
+          </div>
+        </div>
+        <div className="basis-1/2">02</div>
+      </div>
     </>
   )
 }
