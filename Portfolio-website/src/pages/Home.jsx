9e8a7b1be66de168f3e0a710d32ff846d5cd2649
Colin-Harwood <138476341+Colin-Harwood@users.noreply.github.com>
2024-01-07 13:51:23 +0200

    Correctly set up the hero section positioning of objects

--- Portfolio-website/src/pages/Home.jsx
+++ Portfolio-website/src/pages/Home.jsx
@@ -4,16 +4,20 @@ import './Home.css'
 const Home = () => {
   return (
     <>
-      <div className="flex flex-row justify-center align-middle">
-        <div className="flex flex-col items-center basis-1/2 p-4"> 
-          <div className="text-left">
-            <h1 className="text-3xl font-bold">Hi, I'm Colin.</h1>
-            <p>I'm a fullstack web designer based out of South Africa</p>
-            <p>Test</p>
-            <p>Test2</p>
+      <div className="flex flex-col justify-center h-screen">
+        <div className="flex flex-row justify-center">
+          <div className="flex flex-col items-center justify-center basis-1/2 px-50 h-1/3"> 
+            <div className="text-left">
+              <h1 className="text-8xl font-bold">Hi, I'm Colin.</h1>
+              <p className="text-xl">I'm a fullstack web developer based out of South Africa that speciales<br/> in NodeJS, Django, and React. In my free time I enjoy some reading,<br /> videogames and fashion.</p>
+              
+            </div>
           </div>
+          <div className="basis-1/2">02</div>
         </div>
-        <div className="basis-1/2">02</div>
+      </div>
+      <div className="flex justify-center">
+      <h1 className="text-8xl font-bold">My Projects</h1>
       </div>
     </>
   )
