ba0ddc4f64743fb65c8d53f1d73aaca54ae25659
Colin-Harwood <138476341+Colin-Harwood@users.noreply.github.com>
2024-01-07 18:39:42 +0200

    Made the mobile view for the whole page work

--- Portfolio-website/src/pages/Home.jsx
+++ Portfolio-website/src/pages/Home.jsx
@@ -5,15 +5,15 @@ const Home = () => {
   return (
     <>
       <div className="flex flex-col justify-center h-screen">
-        <div className="flex flex-row justify-center">
-          <div className="flex flex-col items-center justify-center basis-1/2 p-7"> 
+        <div className="flex flex-col-reverse md:flex-row justify-center w-full h-full">
+          <div className="flex flex-col items-center justify-center w-full md:w-1/2 p-7 text-center md:text-left">
             <div className="text-left">
-              <h1 className="text-8xl font-bold mt-10">Hi, I'm Colin.</h1>
-              <h1 className="text-4xl font-bold mb-10">I'm a full stack web developer who makes clean and user friendly websites.</h1>
+              <h1 className="lg:text-8xl md:text-5xl sm:text-5xl text-5xl font-bold mt-10">Hi, I'm Colin.</h1>
+              <h1 className="lg:text-4xl md:text-3xl sm:text-3xl text-3xl font-bold mb-10">I'm a full stack web developer who makes clean and user friendly websites.</h1>
               <p className="text-xl">I'm based out of South Africa and specialise in NodeJS, Django, and React.<br/> In my free time I enjoy some reading, videogames and fashion.</p>
             </div>
           </div>
-          <div className="flex flex-col basis-1/2 items-center justify-center content-center">
+          <div className="flex flex-col w-full md:w-1/2 items-center justify-center content-center text-center md:text-left">
             <span className="dot"></span>
             <div className="flex flex-row items-center justify-center mt-10 gap-10">
               <span className="dotSm flex flex-row items-center justify-center"><img src="/gmail.0287b363a72d3c0f445cc724d84f1452.svg" alt="project" width="50px" height="50px" /></span>
@@ -26,12 +26,12 @@ const Home = () => {
       <div className="flex justify-center">
         <h1 className="text-8xl font-bold">My Projects<br/><img src="/image-removebg-preview (6).png" alt="wavyUnderline" width="600px" id="line1"/></h1>
       </div>
-      <div className="grid grid-cols-2 gap-4 mt-5">
+      <div className="grid lg:grid-cols-2 grid-cols-1 gap-4 mt-5">
         <div className="flex flex-col justify-center align-center content-center items-center">
-          <div class="image-wrap">
-            <a href="https://rahmanzeb.com">
-              <img src="/FireShot Capture 015 - Professional Chauffeur Services - Chauffeur - 127.0.0.1.png" alt="Chauffeur Template Image"  class="img-ad"/>
-            </a>
+          <div className="image-wrap">
+            
+              <img src="/FireShot Capture 015 - Professional Chauffeur Services - Chauffeur - 127.0.0.1.png" alt="Chauffeur Template Image"  className="img-ad"/>
+            
           </div>
         </div>
         <div className="flex flex-col justify-center align-center content-center items-center">
@@ -40,17 +40,17 @@ const Home = () => {
           
         </div>
       </div>
-      <div className="grid grid-cols-2 gap-4 mt-5">
-      <div className="flex flex-col justify-center align-center content-center items-center">
+      <div className="grid lg:grid-cols-2 grid-cols-1 gap-4 mt-5">
+        <div className="flex flex-col lg:flex-col justify-center align-center content-center items-center lg:order-first order-last">
           <h1 className="text-5xl">Chauffeur</h1>
           <p>Chauffeur is a chauffeur booking service that allows clients to book a chauffeur service.</p>
           
         </div>
         <div className="flex flex-col justify-center align-center content-center items-center">
-          <div class="image-wrap">
-            <a href="https://rahmanzeb.com">
-              <img src="/FireShot Capture 008 - Ivy Fashion - localhost.png"  class="img-ad"/>
-            </a>
+          <div className="image-wrap">
+            
+              <img src="/FireShot Capture 008 - Ivy Fashion - localhost.png"  className="img-ad"/>
+            
           </div>
         </div>
       </div>
