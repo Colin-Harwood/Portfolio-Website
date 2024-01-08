29d595bfc6d9db631bb0d5ce2b9c0a2c20eda583
Colin-Harwood <138476341+Colin-Harwood@users.noreply.github.com>
2024-01-08 12:07:19 +0200

    Added a skills bar graph to the home page

--- Portfolio-website/src/pages/Home.jsx
+++ Portfolio-website/src/pages/Home.jsx
@@ -8,10 +8,10 @@ const Home = () => {
         <div className="flex flex-col-reverse md:flex-row justify-center w-full h-full">
           <div className="flex flex-col items-center justify-center w-full md:w-1/2 p-9 text-center md:text-left">
             <div className="text-left">
-            <img src="/south-africa (1)-modified.png" alt="wavyUnderline" width="71px" class="absolute"/>
-      <img src="/closed-book-with-bookmark-modified.png" alt="wavyUnderline" width="71px" class="absolute"/>
-      <img src="/console-modified.png" alt="wavyUnderline" width="71px" class="absolute"/>
-      <img src="/sweater-modified.png" alt="wavyUnderline" width="71px" class="absolute"/>
+            <img src="/south-africa (1)-modified.png" alt="wavyUnderline" width="71px" className="absolute"/>
+      <img src="/closed-book-with-bookmark-modified.png" alt="wavyUnderline" width="71px" className="absolute"/>
+      <img src="/console-modified.png" alt="wavyUnderline" width="71px" className="absolute"/>
+      <img src="/sweater-modified.png" alt="wavyUnderline" width="71px" className="absolute"/>
               <h1 className="lg:text-8xl md:text-6xl sm:text-6xl text-6xl font-bold mt-10">Hi, I'm Colin.</h1>
               <h1 className="lg:text-4xl md:text-3xl sm:text-3xl text-3xl font-bold mb-10">I'm a full stack web developer who makes clean and user friendly websites.</h1>
               <p className="text-xl">I'm based out of South Africa and specialise in NodeJS, Django, and React.<br/> In my free time I enjoy some reading, videogames and fashion.</p>
@@ -39,8 +39,8 @@ const Home = () => {
           </div>
         </div>
         <div className="flex flex-col justify-center align-center content-center items-center">
-          <h1 className="text-5xl">Chauffeur</h1>
-          <p>Chauffeur is a chauffeur booking service that allows clients to book a chauffeur service.</p>
+          <h1 className="text-5xl">EasyChauffeur</h1>
+          <p>EasyChauffeur is a chauffeur booking service that allows clients to book a chauffeur service.</p>
           
         </div>
       </div>
@@ -61,7 +61,87 @@ const Home = () => {
       <div className="flex justify-center mt-20">
         <h1 className="text-6xl lg:text-8xl font-bold">My Skills<br/><img src="/image-removebg-preview (6).png" alt="wavyUnderline" width="430px" id="line1"/></h1>
       </div>
-      
+      <div className="flex justify-center mt-10">
+      <div className="flex flex-col content-center rounded-md skill-div">
+        <div className="skill px-4 flex flex-row">
+          <h3 className="pr-7">Django</h3>
+          <div className="skill-bar ">
+            <div className="skill-level" style={{width: '87%'}}></div>
+          </div>
+        </div>
+        <hr/>
+        <div className="skill px-4 flex flex-row">
+          <h3 className="pr-7">Bootstrap</h3>
+          <div className="skill-bar ">
+            <div className="skill-level" style={{width: '85%'}}></div>
+          </div>
+        </div>
+        <hr />
+        <div className="skill px-4 flex flex-row">
+          <h3 className="pr-7">NodeJS</h3>
+          <div className="skill-bar ">
+            <div className="skill-level" style={{width: '82%'}}></div>
+          </div>
+        </div>
+        <hr/>
+        <div className="skill px-4 flex flex-row">
+          <h3 className="pr-7">React</h3>
+          <div className="skill-bar ">
+            <div className="skill-level" style={{width: '80%'}}></div>
+          </div>
+        </div>
+        <hr/>
+        <div className="skill px-4 flex flex-row">
+          <h3 className="pr-7">MongoDb</h3>
+          <div className="skill-bar ">
+            <div className="skill-level" style={{width: '75%'}}></div>
+          </div>
+        </div>
+        <hr/>
+        <div className="skill px-4 flex flex-row">
+          <h3 className="pr-7">JavaScript</h3>
+          <div className="skill-bar ">
+            <div className="skill-level" style={{width: '68%'}}></div>
+          </div>
+        </div>
+        <hr/>
+        <div className="skill px-4 flex flex-row">
+          <h3 className="pr-7">SQL</h3>
+          <div className="skill-bar ">
+            <div className="skill-level" style={{width: '65%'}}></div>
+          </div>
+        </div>
+        <hr/>
+        <div className="skill px-4 flex flex-row">
+          <h3 className="pr-7">Git</h3>
+          <div className="skill-bar ">
+            <div className="skill-level" style={{width: '60%'}}></div>
+          </div>
+        </div>
+        <hr/>
+        <div className="skill px-4 flex flex-row">
+          <h3 className="pr-7">Postman</h3>
+          <div className="skill-bar ">
+            <div className="skill-level" style={{width: '55%'}}></div>
+          </div>
+        </div>
+        <hr/>
+        <div className="skill px-4 flex flex-row">
+          <h3 className="pr-7">Tailwind</h3>
+          <div className="skill-bar ">
+            <div className="skill-level" style={{width: '40%'}}></div>
+          </div>
+        </div>
+        
+        
+      </div>
+      </div>
+      <div className="flex justify-center mt-16 text-center">
+        <h1 className="text-5xl lg:text-7xl font-bold">Interested?<br/>Lets Get In Touch!<br/><img src="/image-removebg-preview (6).png" alt="wavyUnderline" width="650px" id="line1"/></h1>
+      </div>
+      <div className="flex justify-center cont-btn border-white">
+
+      </div>
     </>
   )
 }
