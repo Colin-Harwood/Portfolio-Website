31822f301890e98cf2a45f5a0c799d85914424be
Colin-Harwood <138476341+Colin-Harwood@users.noreply.github.com>
2024-01-08 22:10:28 +0200

    Mobile is finally complete and works on all devices, added links to the buttons in the hero section

--- Portfolio-website/src/pages/Home.jsx
+++ Portfolio-website/src/pages/Home.jsx
@@ -7,14 +7,14 @@ import { BsLinkedin } from "react-icons/bs";
 const Home = () => {
   return (
     <>
-      <div className="flex flex-col justify-center h-screen">
+      <div className="flex flex-col justify-center lg:h-screen h-auto mt-10 lg:mt-0">
         <div className="flex flex-col-reverse md:flex-row justify-center w-full h-full">
           <div className="flex flex-col items-center justify-center w-full md:w-1/2 p-9 text-center md:text-left">
             <div className="text-left">
-            <img src="/south-africa (1)-modified.png" alt="wavyUnderline" width="71px" className="absolute"/>
-      <img src="/closed-book-with-bookmark-modified.png" alt="wavyUnderline" width="71px" className="absolute"/>
-      <img src="/console-modified.png" alt="wavyUnderline" width="71px" className="absolute"/>
-      <img src="/sweater-modified.png" alt="wavyUnderline" width="71px" className="absolute"/>
+            <img src="/south-africa (1)-modified.png" alt="wavyUnderline" width="71px" className="absolute hidden lg:visible"/>
+      <img src="/closed-book-with-bookmark-modified.png" alt="wavyUnderline" width="71px" className="absolute hidden lg:visible"/>
+      <img src="/console-modified.png" alt="wavyUnderline" width="71px" className="absolute hidden lg:visible"/>
+      <img src="/sweater-modified.png" alt="wavyUnderline" width="71px" className="absolute hidden lg:visible"/>
               <h1 className="lg:text-8xl md:text-6xl sm:text-6xl text-6xl font-bold mt-10">Hi, I'm Colin.</h1>
               <h1 className="lg:text-4xl md:text-3xl sm:text-3xl text-3xl font-bold mb-10">I'm a full stack web developer who makes clean and user friendly websites.</h1>
               <p className="text-xl">I'm based out of South Africa and specialise in NodeJS, Django, and React.<br/> In my free time I enjoy some reading, videogames and fashion.</p>
@@ -23,9 +23,15 @@ const Home = () => {
           <div className="flex flex-col w-full md:w-1/2 items-center justify-center content-center text-center md:text-left">
             <span className="dot"></span>
             <div className="flex flex-row items-center justify-center mt-10 gap-10 cont-icons">
+            <a href="mailto:colinharwood0@gmail.com">
               <span className="dotSm flex flex-row items-center justify-center"><img src="/gmail.0287b363a72d3c0f445cc724d84f1452.svg" alt="project" width="50px" height="50px" /></span>
+              </a>
+              <a href="https://github.com/Colin-Harwood">
               <span className="dotSm flex flex-row items-center justify-center"><img src="/github.a4a6a7ae8671bf5749a6aaa4d604fdaa.svg" alt="project" width="50px" height="50px" /></span>
+              </a>
+              <a href="#">
               <span className="dotSm flex flex-row items-center justify-center"><img src="/linkedin.7defd339fd92d3e5fa034b24a5f10ed6.svg" alt="project" width="50px" height="50px" /></span>
+              </a>
             </div>
           </div>
         </div>
