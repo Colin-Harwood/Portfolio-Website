757a2ac8956068c35859a6957ea96c94391d11c2
Colin-Harwood <138476341+Colin-Harwood@users.noreply.github.com>
2024-01-09 19:24:19 +0200

    Updated the text on the EasyChauffeur Page

--- Portfolio-website/src/pages/EasyChauffeur.jsx
+++ Portfolio-website/src/pages/EasyChauffeur.jsx
@@ -11,8 +11,11 @@ const EasyChauffeur = () => {
       </div>
       <div className="grid lg:grid-cols-2 grid-cols-1 gap-4 mt-5">
         <div className="flex flex-col lg:flex-col justify-center align-center content-center items-center ">
-          <h1 className="text-5xl mb-7">Ivy Fashion</h1>
-          <p className='px-10'>Ivy Fashion is a full stack web app built utilising NodeJs, MongoDb, ExpressJS and React. Ivy Fashion allows users to eaily view an assortment of products from different brands in a single location, as well as log in and save items to their wishlist and view the original item on the brands official website.</p>
+          <h1 className="text-5xl mb-7 px-10">Landing, Fleet and Booking Page</h1>
+          <p className='px-10'>The landing page consists of a simple but effective and easy to view layout, giving potential clients an easy understanding of the service as well as specific details to build trust well also supplying reasons as to why use EasyChauffeur over competitors. 
+          <br/>After a client clicks on the button to book a chauffeur they are taken to a clean and simple page with two options, which allows clients to easily navigate and not get confused or misleaded away from the actual service.
+          <br/>The fleet page is a simple page with a list of the cars available for hire, with all of the information easily available ans consumable, leading to clients knowing exactly what options are available.
+          </p>
           <div className="flex flex-row justify-center gap-10">
 
           </div>
@@ -27,12 +30,10 @@ const EasyChauffeur = () => {
           </div>
         </div>
       </div >
-      <div className="flex justify-center mt-7">
-        <h1 className="text-5xl lg:text-8xl font-bold">Payment<br/><img src="/image-removebg-preview (6).png" alt="wavyUnderline" width="440px" id="line1"/></h1>
-      </div>
+      
       <div className="grid lg:grid-cols-2 grid-cols-1 gap-4 mt-5">
         
-          <div class="columns-2">
+          <div class="columns-2 ml-5 order-last lg:order-first">
             <div className="flex flex-col">
               <img src="/FireShot Capture 003 - Professional Chauffeur Services - Chauffeur - 127.0.0.1.png" alt="project"  className="rounded w-full mb-3"/>
               <img src="/FireShot Capture 005 - Professional Chauffeur Services - Chauffeur - 127.0.0.1.png" alt="project"  className="rounded w-full mb-3"/>
@@ -40,19 +41,25 @@ const EasyChauffeur = () => {
             </div>
           </div>
           <div className="flex flex-col lg:flex-col justify-center align-center content-center items-center ">
-            <h1 className="text-5xl mb-7">Ivy Fashion</h1>
-            <p className='px-10'>Ivy Fashion is a full stack web app built utilising NodeJs, MongoDb, ExpressJS and React. Ivy Fashion allows users to eaily view an assortment of products from different brands in a single location, as well as log in and save items to their wishlist and view the original item on the brands official website.</p>
+            <h1 className="text-5xl mb-7">Payment Pages</h1>
+            <p className='px-10'>
+            The payment pages are simple and easy to use, with the client being able to easily input their details 
+            with google maps api helping and automatically complete addresses. Clients are also then shown the route on the interactable map
+            and instantly give prices per vehicle, based on the distance and time of the route, 
+            after confirming their initial details. The payment page is also fully responsive and mobile friendly, allowing clients to easily book a chauffeur on the go.
+            </p>
           </div>
       </div >
-      <div className="flex justify-center mt-16">
-        <h1 className="text-5xl lg:text-8xl font-bold">EasyChauffeur<br/><img src="/image-removebg-preview (6).png" alt="wavyUnderline" width="720px" id="line1"/></h1>
-      </div>
-      <div className="grid lg:grid-cols-2 grid-cols-1 gap-4 mt-5">
+      
+      <div className="grid lg:grid-cols-2 grid-cols-1 gap-4 mt-5 ">
           <div className="flex flex-col lg:flex-col justify-center align-center content-center items-center ml-5">
-            <h1 className="text-5xl mb-7">Ivy Fashion</h1>
-            <p className='px-10'>Ivy Fashion is a full stack web app built utilising NodeJs, MongoDb, ExpressJS and React. Ivy Fashion allows users to eaily view an assortment of products from different brands in a single location, as well as log in and save items to their wishlist and view the original item on the brands official website.</p>
+            <h1 className="text-5xl mb-7">Managing Bookings Pages</h1>
+            <p className='px-10'>
+            The managing bookings pages are simple and easy to use, with the client being able to easily input their email and quickly find al of their bookings.<br/>
+            The client is then able to view all of their bookings and easily change their details, vehicle or cancel a booking at the click of a button.
+            </p>
           </div>
-          <div class="columns-2">
+          <div class="columns-2 mr-5">
             <div className="flex flex-col">
               <img src="/FireShot Capture 006 - Professional Chauffeur Services - Chauffeur - 127.0.0.1.png" alt="project"  className="rounded w-full mb-3"/>
               <img src="/FireShot Capture 007 - Professional Chauffeur Services - Chauffeur - 127.0.0.1.png" alt="project"  className="rounded w-full mb-3"/>
--- Portfolio-website/src/pages/IvyFashion.jsx
+++ Portfolio-website/src/pages/IvyFashion.jsx
@@ -4,7 +4,72 @@ import React from 'react'
 
 const IvyFashion = () => {
   return (
-    <div>IvyFashion</div>
+    <>
+      <div className="flex justify-center mt-7">
+        <h1 className="text-5xl lg:text-8xl font-bold">Ivy Fashion<br/><img src="/image-removebg-preview (6).png" alt="wavyUnderline" width="720px" id="line1"/></h1>
+      </div>
+      <div className="grid lg:grid-cols-2 grid-cols-1 gap-4 mt-5">
+        <div className="flex flex-col lg:flex-col justify-center align-center content-center items-center ">
+          <h1 className="text-5xl mb-7 px-10">Landing Page</h1>
+          <p className='px-10'>The landing page consists of a simple but effective and easy to view layout, giving potential clients an easy understanding of the service as well as specific details to build trust well also supplying reasons as to why use EasyChauffeur over competitors. 
+          <br/>After a client clicks on the button to book a chauffeur they are taken to a clean and simple page with two options, which allows clients to easily navigate and not get confused or misleaded away from the actual service.
+          <br/>The fleet page is a simple page with a list of the cars available for hire, with all of the information easily available ans consumable, leading to clients knowing exactly what options are available.
+          </p>
+          <div className="flex flex-row justify-center gap-10">
+
+          </div>
+          
+        </div>
+        <div className="flex flex-row justify-center align-center content-center items-center p-10 pl-10 pr-10">
+          <img src="/FireShot Capture 015 - Professional Chauffeur Services - Chauffeur - 127.0.0.1.png" alt="project"  className="rounded  w-1/2"/>
+          <div className="flex flex-col">
+          <img src="/FireShot Capture 002 - Professional Chauffeur Services - Chauffeur - 127.0.0.1.png" alt="project"  className="rounded w-full ml-3 mb-3"/>
+            <img src="/FireShot Capture 001 - Professional Chauffeur Services - Chauffeur - 127.0.0.1-copy.png" alt="project"  className="rounded w-full ml-3"/>
+            
+          </div>
+        </div>
+      </div >
+      
+      <div className="grid lg:grid-cols-2 grid-cols-1 gap-4 mt-5">
+        
+          <div class="columns-2 ml-5 order-last lg:order-first">
+            <div className="flex flex-col">
+              <img src="/FireShot Capture 003 - Professional Chauffeur Services - Chauffeur - 127.0.0.1.png" alt="project"  className="rounded w-full mb-3"/>
+              <img src="/FireShot Capture 005 - Professional Chauffeur Services - Chauffeur - 127.0.0.1.png" alt="project"  className="rounded w-full mb-3"/>
+              <img src="/FireShot Capture 004 - Professional Chauffeur Services - Chauffeur - 127.0.0.1.png" alt="project"  className="rounded w-full mb-3"/>
+            </div>
+          </div>
+          <div className="flex flex-col lg:flex-col justify-center align-center content-center items-center ">
+            <h1 className="text-5xl mb-7">Favourites Page</h1>
+            <p className='px-10'>
+            The payment pages are simple and easy to use, with the client being able to easily input their details 
+            with google maps api helping and automatically complete addresses. Clients are also then shown the route on the interactable map
+            and instantly give prices per vehicle, based on the distance and time of the route, 
+            after confirming their initial details. The payment page is also fully responsive and mobile friendly, allowing clients to easily book a chauffeur on the go.
+            </p>
+          </div>
+      </div >
+      
+      <div className="grid lg:grid-cols-2 grid-cols-1 gap-4 mt-5 ">
+          <div className="flex flex-col lg:flex-col justify-center align-center content-center items-center ml-5">
+            <h1 className="text-5xl mb-7">Login and Register Pages</h1>
+            <p className='px-10'>
+            The managing bookings pages are simple and easy to use, with the client being able to easily input their email and quickly find al of their bookings.<br/>
+            The client is then able to view all of their bookings and easily change their details, vehicle or cancel a booking at the click of a button.
+            </p>
+          </div>
+          <div class="columns-2 mr-5">
+            <div className="flex flex-col">
+              <img src="/FireShot Capture 006 - Professional Chauffeur Services - Chauffeur - 127.0.0.1.png" alt="project"  className="rounded w-full mb-3"/>
+              <img src="/FireShot Capture 007 - Professional Chauffeur Services - Chauffeur - 127.0.0.1.png" alt="project"  className="rounded w-full mb-3"/>
+              <img src="/FireShot Capture 016 - Professional Chauffeur Services - Chauffeur - 127.0.0.1.png" alt="project"  className="rounded w-full mb-3"/>
+              <img src="/FireShot Capture 017 - Professional Chauffeur Services - Chauffeur - 127.0.0.1.png" alt="project"  className="rounded w-full mb-3"/>
+
+            </div>
+          </div>
+          
+      </div >
+    </>
   )
 }
 
