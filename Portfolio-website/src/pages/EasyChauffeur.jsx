daf22acb4779c7c2cb85ae778e7e334f25377ad1
Colin-Harwood <138476341+Colin-Harwood@users.noreply.github.com>
2024-01-09 18:07:41 +0200

    Added the images as well got the layout for the EasyChuaffeur page in place

--- Portfolio-website/src/pages/EasyChauffeur.jsx
+++ Portfolio-website/src/pages/EasyChauffeur.jsx
@@ -5,7 +5,65 @@ import React from 'react'
 
 const EasyChauffeur = () => {
   return (
-    <div>EasyChauffeur</div>
+    <>
+      <div className="flex justify-center mt-7">
+        <h1 className="text-5xl lg:text-8xl font-bold">EasyChauffeur<br/><img src="/image-removebg-preview (6).png" alt="wavyUnderline" width="720px" id="line1"/></h1>
+      </div>
+      <div className="grid lg:grid-cols-2 grid-cols-1 gap-4 mt-5">
+        <div className="flex flex-col lg:flex-col justify-center align-center content-center items-center ">
+          <h1 className="text-5xl mb-7">Ivy Fashion</h1>
+          <p className='px-10'>Ivy Fashion is a full stack web app built utilising NodeJs, MongoDb, ExpressJS and React. Ivy Fashion allows users to eaily view an assortment of products from different brands in a single location, as well as log in and save items to their wishlist and view the original item on the brands official website.</p>
+          <div className="flex flex-row justify-center gap-10">
+
+          </div>
+          
+        </div>
+        <div className="flex flex-row justify-center align-center content-center items-center p-10 pl-10 pr-10">
+          <img src="/FireShot Capture 015 - Professional Chauffeur Services - Chauffeur - 127.0.0.1.png" alt="project"  className="rounded  w-1/2"/>
+          <div className="flex flex-col">
+          <img src="/FireShot Capture 002 - Professional Chauffeur Services - Chauffeur - 127.0.0.1.png" alt="project"  className="rounded w-full ml-3 mb-3"/>
+            <img src="/FireShot Capture 001 - Professional Chauffeur Services - Chauffeur - 127.0.0.1-copy.png" alt="project"  className="rounded w-full ml-3"/>
+            
+          </div>
+        </div>
+      </div >
+      <div className="flex justify-center mt-7">
+        <h1 className="text-5xl lg:text-8xl font-bold">Payment<br/><img src="/image-removebg-preview (6).png" alt="wavyUnderline" width="440px" id="line1"/></h1>
+      </div>
+      <div className="grid lg:grid-cols-2 grid-cols-1 gap-4 mt-5">
+        
+          <div class="columns-2">
+            <div className="flex flex-col">
+              <img src="/FireShot Capture 003 - Professional Chauffeur Services - Chauffeur - 127.0.0.1.png" alt="project"  className="rounded w-full mb-3"/>
+              <img src="/FireShot Capture 005 - Professional Chauffeur Services - Chauffeur - 127.0.0.1.png" alt="project"  className="rounded w-full mb-3"/>
+              <img src="/FireShot Capture 004 - Professional Chauffeur Services - Chauffeur - 127.0.0.1.png" alt="project"  className="rounded w-full mb-3"/>
+            </div>
+          </div>
+          <div className="flex flex-col lg:flex-col justify-center align-center content-center items-center ">
+            <h1 className="text-5xl mb-7">Ivy Fashion</h1>
+            <p className='px-10'>Ivy Fashion is a full stack web app built utilising NodeJs, MongoDb, ExpressJS and React. Ivy Fashion allows users to eaily view an assortment of products from different brands in a single location, as well as log in and save items to their wishlist and view the original item on the brands official website.</p>
+          </div>
+      </div >
+      <div className="flex justify-center mt-16">
+        <h1 className="text-5xl lg:text-8xl font-bold">EasyChauffeur<br/><img src="/image-removebg-preview (6).png" alt="wavyUnderline" width="720px" id="line1"/></h1>
+      </div>
+      <div className="grid lg:grid-cols-2 grid-cols-1 gap-4 mt-5">
+          <div className="flex flex-col lg:flex-col justify-center align-center content-center items-center ml-5">
+            <h1 className="text-5xl mb-7">Ivy Fashion</h1>
+            <p className='px-10'>Ivy Fashion is a full stack web app built utilising NodeJs, MongoDb, ExpressJS and React. Ivy Fashion allows users to eaily view an assortment of products from different brands in a single location, as well as log in and save items to their wishlist and view the original item on the brands official website.</p>
+          </div>
+          <div class="columns-2">
+            <div className="flex flex-col">
+              <img src="/FireShot Capture 006 - Professional Chauffeur Services - Chauffeur - 127.0.0.1.png" alt="project"  className="rounded w-full mb-3"/>
+              <img src="/FireShot Capture 007 - Professional Chauffeur Services - Chauffeur - 127.0.0.1.png" alt="project"  className="rounded w-full mb-3"/>
+              <img src="/FireShot Capture 016 - Professional Chauffeur Services - Chauffeur - 127.0.0.1.png" alt="project"  className="rounded w-full mb-3"/>
+              <img src="/FireShot Capture 017 - Professional Chauffeur Services - Chauffeur - 127.0.0.1.png" alt="project"  className="rounded w-full mb-3"/>
+
+            </div>
+          </div>
+          
+      </div >
+    </>
   )
 }
 
